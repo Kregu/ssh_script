abac6eabc04068e4bcf15604ab1a10b0ebd0b30a
keenetic-vladimir <60660172+keenetic-vladimir@users.noreply.github.com>
2020-02-04 18:34:28 +0300

    Update show_ipsec.py

--- show_ipsec.py
+++ show_ipsec.py
@@ -1,11 +1,13 @@
 import paramiko
 from datetime import datetime
-import time
+from time import sleep
+
 host = '192.168.5.1'
 user = 'admin'
 secret = 'password'
 port = 22
 command = 'show ipsec'
+encoding = 'utf-8'
 while True:
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -13,9 +15,9 @@ while True:
     stdin, stdout, stderr = client.exec_command(command)
     data = stdout.read() + stderr.read()
     client.close()
-    with open('data.txt', 'ab') as file_data:
-        file_data.write(data)
-    with open('data.txt', 'a') as file_data:
-        print('Файл Обновлён '+str(datetime.today().strftime("%Y-%m-%d %H:%M:%S")))
-        file_data.write(str(datetime.today().strftime("%Y-%m-%d %H:%M:%S")+str(' Обновлён')))
-    time.sleep(10)
+    with open('/home/vladimir/data.txt', 'a') as file_data:
+        data_time = str(datetime.today().strftime("%Y-%m-%d %H:%M:%S"))
+        print('Файл Обновлён '+ data_time)
+        file_data.write(str('Файл Обновлён' )+ data_time + '\n')
+        file_data.write(data.decode(encoding) + '\n')
+    sleep(5)
